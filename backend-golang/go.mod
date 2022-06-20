f29c57117555da12f4421faf4b358e7912612f20
huuchinh <chinhthaihuu.devplus@gmail.com>
2022-06-20 15:01:21 +0700

    config backend

--- backend-golang/go.mod
+++ backend-golang/go.mod
@@ -1,3 +1,9 @@
-module github.com/testament/server-golang
+module github.com/auth0-blog/go-vr-auth
 
 go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
--- backend-golang/main.go
+++ backend-golang/main.go
@@ -1,19 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	// http routes
-	"net/http"
-	"github.com/julienschmidt/httprouter"
-    // .env
-	"github.com/joho/godotenv"
-	// routes
-	"github.com/conglt10/web-golang/routes"
-)
-
-
+// http routes
+// routes
 
 func main() {
-	
+
 }
